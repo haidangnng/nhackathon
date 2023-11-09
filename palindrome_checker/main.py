e52ad272e84d22275a95a14c6602d24634088243
haidangnng <haidangnng@gmail.com>
2023-11-09 21:06:14 +0100

    feat: palindrome checker

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,45 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+with open("./input.txt", "r") as f:
+    input = f.readlines()
 
-print(input)
+
+def parse_char(char):
+    if char.isalnum():
+        return char.lower()
+    else:
+        return ""
+
+
+def check_palindrome(string):
+    length = len(string)
+    left = 0
+    right = length - 1
+    dict = {}
+
+    while left <= right:
+        left_char = parse_char(string[left])
+        right_char = parse_char(string[right])
+        # if left == right:
+
+        if not left_char:
+            left += 1
+            continue
+
+        if not right_char:
+            right -= 1
+            continue
+
+        if left_char != right_char:
+            print("No, -1")
+            return
+        else:
+            dict[left_char] = dict.get(left_char, 0) + 1
+            left += 1
+            right -= 1
+            continue
+
+    print(f"YeS, {dict.__len__()}")
+    return
+
+
+for line in input:
+    check_palindrome(line)
